733923277f05caae928b103195c9b5edf6e6c7df
franvinci <vincifrancesco101@gmail.com>
2023-10-17 10:31:35 +0200

    FIx START and END

--- SPN_Simulator.py
+++ SPN_Simulator.py
@@ -3,6 +3,8 @@ from tqdm import tqdm
 import pandas as pd
 import pm4py
 from pm4py.algo.conformance.alignments.petri_net import algorithm as alignments
+from pm4py.objects.petri_net.obj import PetriNet
+import os
 
 
 class StochasticPetriNetSimulator:
@@ -12,9 +14,10 @@ class StochasticPetriNetSimulator:
         self.net = net
         self.initial_marking = initial_marking
         self.final_marking = final_marking
+        self.add_start_end_transitions()
         
         if transition_weights == 'equal':
-            self.transition_weights = {t: 1 for t in list(net.transitions)}
+            self.transition_weights = {t: 1 for t in list(self.net.transitions)}
             print('\n', self.transition_weights, '\n')
         if transition_weights == 'frequency':
             if log == []:
@@ -22,17 +25,74 @@ class StochasticPetriNetSimulator:
             else:
                 self.log = log
                 self.transition_weights = self.return_transitions_frequency()
-                for t in list(net.transitions):
-                    if t not in self.transition_weights:
-                        self.transition_weights[t] = 1
                 print('\n', self.transition_weights, '\n')
         if transition_weights == 'manually':
             self.transition_weights = dict()
             print('Insert weight for each transition...')
-            for t in list(net.transitions):
+            for t in list(self.net.transitions):
                 t_w = float(input(str(t) + ": "))
                 self.transition_weights[t] = t_w
             print('\n', self.transition_weights, '\n')
+
+
+    def add_start_end_transitions(self):
+        
+        N = len(self.net.transitions) + len(self.net.places) + 1
+        t_start = PetriNet.Transition(name = 'n'+str(N), label='<START>')
+        self.net.transitions.add(t_start)
+
+        N = len(self.net.transitions) + len(self.net.places) + 1
+        t_end = PetriNet.Transition(name = 'n'+str(N), label='<END>')
+        self.net.transitions.add(t_end)
+
+        N = len(self.net.transitions) + len(self.net.places) + 1
+        new_place_start = PetriNet.Place(name = 'n'+str(N))
+        self.net.places.add(new_place_start)
+
+        N = len(self.net.transitions) + len(self.net.places) + 1
+        new_place_end = PetriNet.Place(name = 'n'+str(N))
+        self.net.places.add(new_place_end)
+
+        places_from = list(self.initial_marking)
+        places_to = list(self.final_marking)
+
+        t_after_start = []
+        for p in places_from:
+            for arc in p.out_arcs:
+                t_after_start.append(arc.target)
+                self.net.arcs.remove(arc)
+            p.out_arcs.clear()
+
+        t_before_end = []
+        for p in places_to:
+            for arc in p.in_arcs:
+                t_before_end.append(arc.source)
+                self.net.arcs.remove(arc)
+            p.in_arcs.clear()
+
+        new_arcs = []
+
+        for p in places_from:
+            new_arcs.append(PetriNet.Arc(p, t_start))
+            
+        for p in places_to:
+            new_arcs.append(PetriNet.Arc(t_end, p))
+
+        new_arcs.append(PetriNet.Arc(t_start, new_place_start))
+        new_arcs.append(PetriNet.Arc(new_place_end, t_end))
+
+        for t in t_after_start:
+            new_arcs.append(PetriNet.Arc(new_place_start, t))
+
+        for t in t_before_end:
+            new_arcs.append(PetriNet.Arc(t, new_place_end))
+
+        for arc in new_arcs:
+            self.net.arcs.add(arc)
+
+        pm4py.write_pnml(self.net, self.initial_marking, self.final_marking, 'net_startEnd.pnml')
+        self.net, self.initial_marking, self.final_marking = pm4py.read_pnml('net_startEnd.pnml')
+        os.remove('net_startEnd.pnml')
 
 
     def return_transitions_frequency(self):
@@ -88,13 +148,13 @@ class StochasticPetriNetSimulator:
         tkns = list(self.initial_marking)
         enabled_transitions = self.return_enabled_transitions(tkns)
         t_fired = self.return_fired_transition(enabled_transitions)
-        if t_fired.label:
+        if t_fired.label and (t_fired.label not in ['<START>', '<END>']):
             trace_sim.append(t_fired.label)
         tkns = self.update_markings(tkns, t_fired)
         while set(tkns) != set(self.final_marking):
             enabled_transitions = self.return_enabled_transitions(tkns)
             t_fired = self.return_fired_transition(enabled_transitions)
-            if t_fired.label:
+            if t_fired.label and (t_fired.label not in ['<START>', '<END>']):
                 trace_sim.append(t_fired.label)
             tkns = self.update_markings(tkns, t_fired)
         return trace_sim
